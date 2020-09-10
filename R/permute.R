f1215f26444634ab39aa80f14669be7ff8635258
dc165 <51100406+dc165@users.noreply.github.com>
2020-09-10 08:12:27 -0400

    permutation and bootstrap methods

---
+++ R/permute.R
@@ -0,0 +1,46 @@
+# Permutations of hypervolumes from two input hypervolumes
+permute <- function(name, hv1, hv2, n = 50, cores = 1, verbose = TRUE) {
+  # Check if cluster registered to doparallel backend exists
+  exists_cluster = TRUE
+  if(cores > 1 & getDoParWorkers() == 1) {
+    # If no cluster is registered, create a new one based on use input
+    cl = makeCluster(cores)
+    clusterEvalQ(cl, {
+      library(hypervolume)
+      source('Utils.R')
+    })
+    registerDoParallel(cl)
+    exists_cluster = FALSE
+  }
+  
+  # Create folder to store permuted hypervolumes
+  dir.create(file.path('./Objects', name))
+  if(verbose) {
+    pb = progress_bar$new(total = n)
+  }
+  
+  # Create n pairs of hypervolumes by permuting combined data of hv1 and hv2
+  foreach(i = 1:n, .combine = c) %dopar% {
+    combined_data = rbind(hv1@Data, hv2@Data)
+    perm_idx = sample(1:nrow(combined_data), nrow(hv1@Data))
+    h1 = copy_param_hypervolume(hv1, combined_data[perm_idx,], name = "hv1")
+    h2 = copy_param_hypervolume(hv2, combined_data[-1 * perm_idx,], name = "hv2")
+    
+    subdir = file.path("./Objects", name, paste0("permutation", as.character(i)))
+    dir.create(subdir)
+    name1 = paste0(h1@Name, '.rds')
+    name2 = paste0(h2@Name, '.rds')
+    saveRDS(h1, file.path('./Objects', name, paste0("permutation", as.character(i)), name1))
+    saveRDS(h2, file.path('./Objects', name, paste0("permutation", as.character(i)), name2))
+    if(verbose) {
+      pb$tick()
+    }
+  }
+  
+  # If a cluster was created for this specific function call, close cluster and register sequential backend
+  if(!exists_cluster) {
+    stopCluster(cl)
+    registerDoSEQ()
+  }
+  return(file.path(getwd(), 'Objects', name))
+}
---
+++ R/resample.R
@@ -0,0 +1,26 @@
+resample <- function(name, hv, method, n = 10, points_per_resample = 'sample_size', seq = 3:nrow(hv@Data), k = 5, cores = 1, verbose = TRUE, mu = NULL, sigma = NULL, cols_to_bias = 1:ncol(hv@Data)) {
+  # Check for valid inptus
+  if (n <= 0) {
+    stop("Invalid value for n")
+  } else if (points_per_resample != "sample size" & points_per_resample <= 0) {
+    stop("Invalid value for points_per_resample")
+  } else if (seq[1] <= 0) {
+    stop("Invalid input for seq")
+  } else if (method == 'biased bootstrap' & (length(mu) != length(sigma) | length(mu) != length(cols_to_bias))) {
+    stop("mu, sigma, and cols_to_bias must have same length")
+  } else if (cores < 1) {
+    stop("cores must be greater than or equal to 1")
+  }
+  
+  # Create Objects folder in current working directory if one doesn't already exist, then call functions
+  dir.create('./Objects', showWarnings = FALSE)
+  if (method == 'bootstrap') {
+    return(bootstrap(name, hv, n, points_per_resample, cores, verbose))
+  } else if (method == 'bootstrap seq') {
+    return(bootstrap_seq(name, hv, n, points_per_resample, seq, cores, verbose))
+  } else if (method == 'biased bootstrap') {
+    return(sampling_bias_bootstrap(name, hv, n, points_per_resample, cores, verbose, mu, sigma, cols_to_bias))
+  } else if (method == "k_split") {
+    return(k_split(name, hv, k, cores, verbose))
+  }
+}
