7e7774cba75c83a85686e156e7b88cda97be1cd6
dc165 <51100406+dc165@users.noreply.github.com>
2023-08-18 17:08:18 -0400

    Update checks before calling hypervolume_sample. Remove k_split from package

--- R/hypervolume_resample.R
+++ R/hypervolume_resample.R
@@ -1,13 +1,15 @@
-hypervolume_resample <- function(name, hv, method, n = 10, points_per_resample = 'sample_size', seq = 3:nrow(hv@Data), k = 5, cores = 1, verbose = TRUE, to_file = TRUE, mu = NULL, sigma = NULL, cols_to_weigh = 1:ncol(hv@Data), weight_func = NULL) {
+hypervolume_resample <- function(name, hv, method, n = 10, points_per_resample = 'sample_size', seq = 3:nrow(hv@Data), cores = 1, verbose = TRUE, to_file = TRUE, mu = NULL, sigma = NULL, cols_to_weigh = 1:ncol(hv@Data), weights = NULL) {
   # Check for valid inputs
   if (n <= 0) {
     stop("Invalid value for n")
   } else if (points_per_resample != "sample size" & points_per_resample <= 0) {
     stop("Invalid value for points_per_resample")
-  } else if (seq[1] <= 0) {
+  } else if (any(seq <= 0)) {
     stop("Invalid input for seq")
-  } else if (method == 'weighted bootstrap' & is.null(weight_func) & (length(mu) != length(sigma) | length(mu) != length(cols_to_weigh))) {
+  } else if (method == 'weighted bootstrap' & is.null(weights) & (length(mu) != length(sigma) | length(mu) != length(cols_to_weigh))) {
     stop("mu, sigma, and cols_to_weigh must have same length")
+  } else if (method == 'weighted bootstrap' & length(weights) != nrow(hv@Data)) {
+    stop("Number of weights must match size of data.")
   } else if (cores < 1) {
     stop("cores must be greater than or equal to 1")
   }
@@ -19,8 +21,6 @@ hypervolume_resample <- function(name, hv, method, n = 10, points_per_resample =
   } else if (method == 'bootstrap seq') {
     return(bootstrap_seq(name, hv, n, seq, cores, verbose))
   } else if (method == 'weighted bootstrap') {
-    return(weighted_bootstrap(name, hv, n, points_per_resample, cores, verbose, to_file, mu, sigma, cols_to_weigh, weight_func))
-  } else if (method == "k_split") {
-    return(k_split(name, hv, k, cores, verbose))
+    return(weighted_bootstrap(name, hv, n, points_per_resample, cores, verbose, to_file, mu, sigma, cols_to_weigh, weights))
   }
 }
